c4a6f21078631f2f0a2bc276e654da2d9d43ed01
Lastzlo <z.last.zlo@gmail.com>
2021-06-23 20:46:43 +0300

    test add ForLoopTest.printlnText10Times that print Text 10 times

---
+++ src/test/java/ForLoopTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class ForLoopTest {
+    @Test
+    void printlnText10Times() {
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Text");
+        }
+
+    }
+}
